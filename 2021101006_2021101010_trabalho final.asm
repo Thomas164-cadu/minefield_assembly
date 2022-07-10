4d523367b34c2f5c3f3ad049388930bc47a4fd95
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-07-10 19:04:35 -0300

    funcionando o print

--- 2021101006_2021101010_trabalho final.asm
+++ 2021101006_2021101010_trabalho final.asm
@@ -3,38 +3,39 @@ menu:		.asciz	"\nCampo Minado \nQual o tamanho do campo que deseja jogar? \n1 - 
 inputFail:	.asciz "\nO valor selecionado nï¿½o existe, favor imprmir novamente\n"
 matriz:		.word 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9
 cerquilha:	.asciz "#"
+quebra_linha: .asciz "\n"
 	.text
 main:
-	la s6, matriz # s6 ï¿½ endereï¿½o da matriz
-	li s7, 4
-	jal escolher_matriz
-	jal mostrar_matriz
-	nop
+	la s6, matriz # carrega endereÃ§o da matriz em s6
+	li s7, 4 # carrega imediato 4 em s7
+	jal escolher_matriz # chama a escolha da matriz e salva em s0 o tamanho de posicoes do vet e em t0 o tamanho do lado da matriz
+	jal mostrar_matriz # mostra a matriz de acordo com o tamanho escolhido
+	nop # encerra o programa
 
 # escolher matriz, talvez mudar
 escolher_matriz:
-	#O 4 representa o indíce do tipo de comando de impressão é salvo no a7 
-	#Depois o a0 receba o endereço de memória da string e o sistema imprima no console
+	# O 4 representa o indÃ­ce do tipo de comando de impressÃ£o salvo no a7 
+	# Depois o a0 receba o endereÃ§o de memÃ³ria da string e o sistema imprima no console
 	li	a7, 4
 	la	a0, menu
 	ecall
-	#Aqui é a mesma lógica, o número 5 representa um comando para ler o input do usuário
+	# Aqui segue a mesma lÃ³gica, o nÃºmero 5 representa um comando para ler o input do usuÃ¡rio
 	li	a7, 5
 	ecall
-	#O valor retornado será armazenado no temporário a0, passamos ele para t0
+	# O valor a retornado Ã© armazenado no temporÃ¡rio a0, passamos ele para s0
 	mv 	s0, a0
 
 	# Verificando se o input corresponde aos valores fornecidos
 	li	t1, 1
 	li	t2, 2
 	li	t3, 3
-	beq	s0, t1, carregar_dados_oito # parametro em s0, size
-	beq	s0, t2, carregar_dados_dez
-	beq	s0, t3, carregar_dados_doze
+	beq	s0, t1, carregar_dados_oito # salva o size em s0 e da ret
+	beq	s0, t2, carregar_dados_dez # salva o size em s0 e da ret
+	beq	s0, t3, carregar_dados_doze # salva o size em s0 e da ret
 	li	a7, 4
 	la	a0, inputFail
 	ecall
-	j	escolher_matriz
+	j escolher_matriz
 	
 carregar_dados_oito: 
 	li t0, 8
@@ -50,18 +51,26 @@ carregar_dados_doze:
 	ret
 
 mostrar_matriz:
-	mul t2, s7, s0
-	add t2, t2, s6 # t3 o max do for
+	# carrega o ultimo endereco de memoria do vet em t2
+	mul t2, s7, s0 # t2 = s7 * s0 / t2 = 4 * size / 4 * 64 = 256
+	add t2, t2, s6 # t2 = memIni + 256
+	add t6, s6, zero # t6 = memIni
 for_mostrar_matriz:
-	beq t0, t2, fim_for_mostrar_matriz
-	
+	beq t6, t2, fim_for_mostrar_matriz # if (memIni = t2)
+	mul t3, s7, t0 # t3 = s7 * t0 = 4 * 8  = 24
+	add t3, t3, t6 # t3 = memIni + 24
+for_dentro_mostrar_matriz:
+	beq t6, t3, fim_for_dentro_mostrar_matriz
 	li a7, 4
 	la a0, cerquilha
 	ecall
-	
-	add t0, t0, s7
+	add t6, t6, s7 # ++
+	j for_dentro_mostrar_matriz
+fim_for_dentro_mostrar_matriz:
+	li a7, 4
+	la a0, quebra_linha
+	ecall
 	j for_mostrar_matriz
-	
 fim_for_mostrar_matriz:
 	ret
 
