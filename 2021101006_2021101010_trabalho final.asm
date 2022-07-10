5c1d9be690baa2a78be9b6cbbf4a12e809d8f7fd
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-07-10 17:45:01 -0300

    funcionou o print de toda a matriz

--- 2021101006_2021101010_trabalho final.asm
+++ 2021101006_2021101010_trabalho final.asm
@@ -1,10 +1,18 @@
 	.data
-menu:	.asciz	"\nCampo Minado \nQual o tamanho do campo que deseja jogar? \n1 - 8x8 \n2 - 10x10 \n3 - 12x12\n"
-inputFail:	.asciz "\nO valor selecionado não existe, favor imprmir novamente\n"
+menu:		.asciz	"\nCampo Minado \nQual o tamanho do campo que deseja jogar? \n1 - 8x8 \n2 - 10x10 \n3 - 12x12\n"
+inputFail:	.asciz "\nO valor selecionado nï¿½o existe, favor imprmir novamente\n"
+matriz:		.word 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9
+cerquilha:	.asciz "#"
 	.text
 main:
+	la s6, matriz # s6 ï¿½ endereï¿½o da matriz
+	li s7, 4
+	jal escolher_matriz
+	jal mostrar_matriz
+	nop
 
-imprime_menu:
+# escolher matriz, talvez mudar
+escolher_matriz:
 	#O 4 representa o indíce do tipo de comando de impressão é salvo no a7 
 	#Depois o a0 receba o endereço de memória da string e o sistema imprima no console
 	li	a7, 4
@@ -14,24 +22,50 @@ imprime_menu:
 	li	a7, 5
 	ecall
 	#O valor retornado será armazenado no temporário a0, passamos ele para t0
-	mv 	t0, a0
-	
-verifica_opcao:
-	#Verificando se o input corresponde aos valores fornecidos
+	mv 	s0, a0
+
+	# Verificando se o input corresponde aos valores fornecidos
 	li	t1, 1
 	li	t2, 2
 	li	t3, 3
-	beq	t0, t1, matriz_oito
-	beq	t0, t2, matriz_dez
-	beq	t0, t3, matriz_doze
+	beq	s0, t1, carregar_dados_oito # parametro em s0, size
+	beq	s0, t2, carregar_dados_dez
+	beq	s0, t3, carregar_dados_doze
 	li	a7, 4
 	la	a0, inputFail
 	ecall
-	j	imprime_menu
+	j	escolher_matriz
 	
-matriz_oito:
-	nop
-matriz_dez:
-	nop
-matriz_doze:
-	nop
+carregar_dados_oito: 
+	li t0, 8
+	mul s0, t0, t0
+	ret
+carregar_dados_dez:
+	li t0, 10
+	mul s0, t0, t0
+	ret
+carregar_dados_doze:
+	li t0, 12
+	mul s0, t0, t0
+	ret
+
+mostrar_matriz:
+	mul t2, s7, s0
+	add t2, t2, s6 # t3 o max do for
+for_mostrar_matriz:
+	beq t0, t2, fim_for_mostrar_matriz
+	
+	li a7, 4
+	la a0, cerquilha
+	ecall
+	
+	add t0, t0, s7
+	j for_mostrar_matriz
+	
+fim_for_mostrar_matriz:
+	ret
+
+
+	
+	
+	
