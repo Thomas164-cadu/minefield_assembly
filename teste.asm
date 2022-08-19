35ba33ad968746e35270f5ec2ea56ed30ca40cf2
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-08-18 22:21:13 -0300

    Update teste.asm

--- teste.asm
+++ teste.asm
@@ -57,6 +57,10 @@ main:
 	la a0, campo
 	mv a1, s0
 	jal INSERE_BOMBA
+
+	la a0, campo
+	mv a1, s0
+	jal calcula_bombas
 	
 	# remover
 	add a0, s11, zero
@@ -370,3 +374,134 @@ PSEUDO_RAND:
 	mul  a0, a0, s2		    	
 EH_POSITIVO:	
 		ret				
+
+calcula_bombas:
+	# primeiro setamos as variaveis para iniciar o for
+	mul a2, a1, a1
+	li t4, 4
+	mul a3, a2, t4
+	add a4, a3, a0
+	mv t1, a0
+	li s9, 9
+	li t0, 1
+	
+for_calcula_bombas:
+	beq t1, a4, fim_calcula_bombas
+	mv t3, t1
+	lw t2, (t1)
+	add t1, t1, t4
+	beq t2, s9, valida_bomba
+	j for_calcula_bombas
+	# apos verificar se existe a bomba na posicao ele volta para o for se nao for, caso for, valida
+	
+valida_bomba:
+	#pulamos para a pr�xima posi��o
+	add a3, t3, t4
+	lw t5, (a3)	
+	#verificamos se nessa posi��o existe uma bomba se nao vamos para fun��o de verifica��o
+	bne t5, s9, right_spot
+next_spot_one:
+	sub a3, t3, t4
+	lw t5, (a3)
+	bne t5, s9, left_spot
+next_spot_two:
+	mul a5, a1, t4
+	sub a3, t3, a5
+	lw t5, (a3)
+	blt a3, a0, next_spot_five
+	bne t5, s9, top_spot
+next_spot_three:
+	# esse e no as pra nao perder o top
+	sub a5, a3, t4
+	lw t5, (a5)
+	bne t5, s9, top_left_spot
+next_spot_four:
+	add a3, a3, t4
+	lw t5, (a3)
+	bne t5, s9, top_right_spot
+next_spot_five:
+	mul a5, a1, t4
+	add a3, t3, a5
+	lw t5, (a3)
+	bgt a3, a4, for_calcula_bombas
+	bne t5, s9, bottom_spot
+next_spot_six:
+	sub a5, a3, t4
+	lw t5, (a5)
+	bne t5, s9, bottom_left_spot
+next_spot_seven:
+	add a3, a3, t4
+	lw t5, (a3)
+	bne t5, s9, bottom_right_spot
+next_spot_eight:
+	j for_calcula_bombas
+	
+
+# POSIcoES
+right_spot:
+	# na funcao de verificacao, fazemos os calculos para verificar se nao se trata de um canto
+	bgt a3, a4, next_spot_one
+	div t6, a3, a0
+	div t6, t6, t4
+	rem t6, t6, a1 
+	beq t6, zero, next_spot_one
+	# apos a verificacao de canto aumentamos a contagem de bombas
+	add t5, t5, t0
+	sw t5, (a3)
+	# e voltamos para a proxima posicao ao redor da bomba
+	j next_spot_one
+left_spot:
+	blt a3, a0, next_spot_two
+	div t6, a3, a0
+	div t6, t6, t4
+	rem t6, t6, a1 
+	beq t6, t0, next_spot_two
+	add t5, t5, t0
+	sw t5, (a3)
+	j next_spot_two
+top_spot:
+	add t5, t5, t0
+	sw t5, (a3)
+	j next_spot_three
+top_left_spot:
+	blt a5, a0, next_spot_four
+	div t6, a5, a0
+	div t6, t6, t4
+	rem t6, t6, a1 
+	beq t6, t0, next_spot_four
+	add t5, t5, t0
+	sw t5, (a5)
+	j next_spot_four
+top_right_spot:
+	bgt a3, a4, next_spot_five
+	div t6, a3, a0
+	div t6, t6, t4
+	rem t6, t6, a1 
+	beq t6, zero, next_spot_five
+	add t5, t5, t0
+	sw t5, (a5)
+	j next_spot_five
+bottom_spot:
+	add t5, t5, t0
+	sw t5, (a3)
+	j next_spot_six
+bottom_left_spot:
+	div t6, a5, a0
+	div t6, t6, t4
+	rem t6, t6, a1 
+	beq t6, t0, next_spot_seven
+	add t5, t5, t0
+	sw t5, (a5)
+	j next_spot_seven
+bottom_right_spot:
+	bgt a3, a4, next_spot_eight
+	div t6, a3, a0
+	div t6, t6, t4
+	rem t6, t6, a1 
+	beq t6, zero, next_spot_eight
+	add t5, t5, t0
+	sw t5, (a5)
+	j next_spot_eight
+					
+fim_calcula_bombas:
+	ret
