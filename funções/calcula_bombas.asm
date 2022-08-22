f0c736032468b0e9f568fc2974a86aefdc7f67fa
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-08-21 22:08:29 -0300

    sem comentarios

--- funções/calcula_bombas.asm
+++ funções/calcula_bombas.asm
@@ -1,99 +1,134 @@
 
-# a0 => pos inicial do vetor
-# a1 => tamanho do lado do campo
 calcula_bombas:
-    li t0, 1 # 1
-    li s9, 9 # 9
-    add s7, s7, zero # 4
+    li s9, 9 
+    add s7, s7, zero 
     mul t1, a1, a1
     mul t1, t1, s7
     add t1, t1, a0
-    mv t2, a0 
+    mv t0, a0 
 for_calcula_bombas:
-    beq t1, t2, fim_calcula_bombas
-    lw t3, (t2)
-    add t2, t2, s7 # incrementa for
-    beq t3, s9, valida_bomba
+    beq t0, t1, fim_calcula_bombas
+    lw t3, (t0)
+    beq t3, s9, valida_bomba 
+continua_for_calcula_bombas:
+    add t0, t0, s7 
     j for_calcula_bombas
 valida_bomba:
-    j valida_direita
 valida_direita:
-    add t4, t2, s7 # j = i + 1;
-    lw t5, (t4) # vetor[j] 
-    beq t5, s9, valida_esquerda # if vetor[j] != 9
-    # bgt t4, t1, valida_esquerda
-    sub t6, t4, a0
-    div t6, t6, s7
-    rem t6, t6, a1
-    beq t6, zero, valida_esquerda # if j % 12 != 0
-    add t5, t5, t0
+    add t4, t0, s7 
+    lw t5, (t4) 
+    bne t5, s9, continua_valida_direita 
+    j valida_esquerda 
+continua_valida_direita:
+    sub s5, t4, a0
+	div s5, s5, s7
+    rem t6, s5, a1 
+    bne t6, zero, salva_valida_direita 
+    j valida_esquerda
+salva_valida_direita:
+    addi t5, t5, 1
     sw t5, (t4)
 valida_esquerda:
-    sub t4, t2, s7 # j = i - 1
-    lw t5, (t4) # vetor[j] 
-    beq t5, s9, valida_por_cima # if vetor[j] != 9
-    # blt t4, a0, valida_acima
-    sub t6, t4, a0
-    div t6, t6, s7
-    rem t6, t6, a1
-    sub s5, a1, t0
-    beq t6, s5, valida_por_cima # j % 12 != 11
-    add t5, t5, t0
+    sub t4, t0, s7 
+    lw t5, (t4) 
+    bne t5, s9, continua_valida_esquerda
+    j valida_por_cima 
+continua_valida_esquerda:
+    sub s5, t4, a0
+	div s5, s5, s7
+    rem t6, s5, a1 
+    addi s5, a1, -1
+    bne t6, s5, salva_valida_esquerda 
+    j valida_por_cima
+salva_valida_esquerda:
+    addi t5, t5, 1
     sw t5, (t4)
 valida_por_cima:
-    mul t6, a1, s7 # auxaux = 8 x 4 = 64
-    sub t4, t2, t6 # aux = auxaux - 64		i-12
-    lw t5, (t4) # vetor[aux]
-    blt t4, a0, valida_acima
-valida_acima:
-    beq t5, s9, valida_abaixo # vetor[aux] != 9
-    add t5, t5, t0
-    sw t5, (t4)
-valida_diagonal_esquerda_cima:
-
-    beq t5, s9, valida_diagonal_esquerda_baixo
-    blt t4, a0, valida_diagonal_esquerda_baixo
-    add t5, t5, t0
-    sw t5, (t4)
-valida_diagonal_direita_cima:
     mul t6, a1, s7
-    sub t6, t6, s7
-    sub t4, t2, t6
-    lw t5, (t4)
-    beq t5, s9, valida_diagonal_direita_baixo
-    blt t4, a0, valida_diagonal_direita_baixo
-    add t5, t5, t0
-    sw t5, (t4)
-
+    sub t6, t0, t6 
+    bgt t6, a0, valida_meio_cima
+    j valida_por_baixo
+valida_meio_cima:
+    lw t5, (t6)
+    bne t5, s9, continua_valida_por_cima
+    j valida_esquerda_cima 
+continua_valida_por_cima:
+    addi t5, t5, 1
+    sw t5, (t6)
+valida_esquerda_cima:
+    sub s1, t6, s7 
+    bgt s1, zero, continua_continua_valida_esquerda_cima
+    j valida_direita_cima
+continua_continua_valida_esquerda_cima:
+    lw t5, (s1)
+    bne t5, s9, continua_valida_esquerda_cima
+    j valida_direita_cima
+continua_valida_esquerda_cima:
+    addi s5, a1, -1
+    sub s8, s1, a0
+	div s8, s8, s7
+    rem s2, s8, a1
+    bne s2, s5, salva_valida_esquerda_cima
+    j valida_direita_cima
+salva_valida_esquerda_cima:
+    addi t5, t5, 1
+    sw t5, (s1)
+valida_direita_cima:
+    add s1, t6, s7 
+    lw t5, (s1)
+    bne t5, s9, continua_valida_direita_cima
+    j valida_por_baixo
+continua_valida_direita_cima:
+    sub s8, s1, a0
+	div s8, s8, s7
+    rem s2, s8, a1
+    bne s2, zero, salva_valida_direita_cima
+    j valida_por_baixo
+salva_valida_direita_cima:
+    addi t5, t5, 1
+    sw t5, (s1)
 valida_por_baixo:
-
-valida_abaixo:
     mul t6, a1, s7
-    add t4, t2, t6
-    lw t5, (t4)
-    beq t5, s9, valida_diagonal_esquerda_cima
-    blt t4, a0, valida_diagonal_esquerda_cima
-    add t5, t5, t0
-    sw t5, (t4)
-valida_diagonal_esquerda_baixo:
-    mul t6, a1, s7
-    sub t6, t6, s7
-    add t4, t2, t6
-    lw t5, (t4)
-    beq t5, s9, valida_diagonal_direita_cima
-    bgt t4, t1, valida_diagonal_direita_cima
-    add t5, t5, t0
-    sw t5, (t4)
-valida_diagonal_direita_baixo:
-    mul t6, a1, s7
-    add t6, t6, s7
-    add t4, t2, t6
-    lw t5, (t4)
-    beq t5, s9, termina_validacao
-    bgt t4, t1, termina_validacao
-    add t5, t5, t0
-    sw t5, (t4)
-termina_validacao:
-    j for_calcula_bombas
+    add t6, t0, t6 
+    blt t6, t1, valida_meio_baixo
+    j continua_for_calcula_bombas
+valida_meio_baixo:
+    lw t5, (t6)
+    bne t5, s9, continua_valida_meio_baixo
+    j valida_esquerda_baixo 
+continua_valida_meio_baixo:
+    addi t5, t5, 1
+    sw t5, (t6)
+valida_esquerda_baixo:
+    sub s1, t6, s7 
+    lw t5, (s1)
+    bne t5, s9, continua_valida_esquerda_baixo
+    j valida_direita_baixo
+continua_valida_esquerda_baixo:
+    addi s5, a1, -1
+    sub s8, s1, a0
+	div s8, s8, s7
+    rem s2, s8, a1
+    bne s2, s5, salva_valida_esquerda_baixo
+    j valida_direita_baixo
+salva_valida_esquerda_baixo:
+    addi t5, t5, 1
+    sw t5, (s1)
+valida_direita_baixo:
+    add s1, t6, s7
+    blt t1, s1, continua_for_calcula_bombas
+    lw t5, (s1)
+    bne t5, s9, continua_valida_direita_baixo
+    j continua_for_calcula_bombas
+continua_valida_direita_baixo:
+    sub s5, s1, a0
+	div s5, s5, s7
+    rem s2, s5, a1
+    bne s2, zero, salva_valida_direita_baixo
+    j continua_for_calcula_bombas
+salva_valida_direita_baixo:
+    addi t5, t5, 1
+    sw t5, (s1)
+    j continua_for_calcula_bombas
 fim_calcula_bombas:
     ret
--- teste1.asm
+++ teste1.asm
@@ -62,16 +62,6 @@ main:
 	mv a1, s0
 	jal calcula_bombas
 	
-	# remover
-	add a0, s11, zero
-	add a1, s10, zero
-	la a2, campo
-	la a3, indices
-	jal mostra_campo
-	# remover
-
-	# calcula bombas
-	
 	add a0, s11, zero
 	la a1, inputFail
 	jal menu_de_jogadas
@@ -406,13 +396,9 @@ PSEUDO_RAND:
 	mul  a0, a0, s2		    	
 EH_POSITIVO:	
 		ret
-
-# CALCULA A MERDA DAS BOMBAS DESSE CAMPO DE BOSTA COM ESSE PROFE CUZÃO NESSA UNIVERSIDADE DE LIXO
-# a0 => pos inicial do vetor
-# a1 => tamanho do lado do campo
 calcula_bombas:
-    li s9, 9 # 9
-    add s7, s7, zero # 4
+    li s9, 9 
+    add s7, s7, zero 
     mul t1, a1, a1
     mul t1, t1, s7
     add t1, t1, a0
@@ -420,54 +406,54 @@ calcula_bombas:
 for_calcula_bombas:
     beq t0, t1, fim_calcula_bombas
     lw t3, (t0)
-    beq t3, s9, valida_bomba # if (vetor[i] == 9) 
+    beq t3, s9, valida_bomba 
 continua_for_calcula_bombas:
-    add t0, t0, s7 # incrementa for
+    add t0, t0, s7 
     j for_calcula_bombas
 valida_bomba:
 valida_direita:
-    add t4, t0, s7 # j = i + 1
-    lw t5, (t4) # vetor[j]
-    bne t5, s9, continua_valida_direita # vetor[j] != 9
+    add t4, t0, s7 
+    lw t5, (t4) 
+    bne t5, s9, continua_valida_direita 
     j valida_esquerda 
 continua_valida_direita:
     sub s5, t4, a0
 	div s5, s5, s7
-    rem t6, s5, a1 # j % lado
-    bne t6, zero, salva_valida_direita # j % 12 != 0
+    rem t6, s5, a1 
+    bne t6, zero, salva_valida_direita 
     j valida_esquerda
 salva_valida_direita:
     addi t5, t5, 1
     sw t5, (t4)
 valida_esquerda:
-    sub t4, t0, s7 # j = i - 1
-    lw t5, (t4) # vetor[j]
+    sub t4, t0, s7 
+    lw t5, (t4) 
     bne t5, s9, continua_valida_esquerda
-    j valida_por_cima # vetor[j] != 9
+    j valida_por_cima 
 continua_valida_esquerda:
     sub s5, t4, a0
 	div s5, s5, s7
-    rem t6, s5, a1 # j % 12
+    rem t6, s5, a1 
     addi s5, a1, -1
-    bne t6, s5, salva_valida_esquerda # j % 12 != 11
+    bne t6, s5, salva_valida_esquerda 
     j valida_por_cima
 salva_valida_esquerda:
     addi t5, t5, 1
     sw t5, (t4)
 valida_por_cima:
     mul t6, a1, s7
-    sub t6, t0, t6 # aux = i - 12;
+    sub t6, t0, t6 
     bgt t6, a0, valida_meio_cima
     j valida_por_baixo
 valida_meio_cima:
     lw t5, (t6)
     bne t5, s9, continua_valida_por_cima
-    j valida_esquerda_cima # vetor[aux] != 9
+    j valida_esquerda_cima 
 continua_valida_por_cima:
     addi t5, t5, 1
     sw t5, (t6)
 valida_esquerda_cima:
-    sub s1, t6, s7 # j = aux - 1
+    sub s1, t6, s7 
     bgt s1, zero, continua_continua_valida_esquerda_cima
     j valida_direita_cima
 continua_continua_valida_esquerda_cima:
@@ -485,7 +471,7 @@ salva_valida_esquerda_cima:
     addi t5, t5, 1
     sw t5, (s1)
 valida_direita_cima:
-    add s1, t6, s7 # j = aux + 1
+    add s1, t6, s7 
     lw t5, (s1)
     bne t5, s9, continua_valida_direita_cima
     j valida_por_baixo
@@ -500,18 +486,18 @@ salva_valida_direita_cima:
     sw t5, (s1)
 valida_por_baixo:
     mul t6, a1, s7
-    add t6, t0, t6 # aux = i + 12;
+    add t6, t0, t6 
     blt t6, t1, valida_meio_baixo
     j continua_for_calcula_bombas
 valida_meio_baixo:
     lw t5, (t6)
     bne t5, s9, continua_valida_meio_baixo
-    j valida_esquerda_baixo # vetor[aux] != 9
+    j valida_esquerda_baixo 
 continua_valida_meio_baixo:
     addi t5, t5, 1
     sw t5, (t6)
 valida_esquerda_baixo:
-    sub s1, t6, s7 # j = aux - 1
+    sub s1, t6, s7 
     lw t5, (s1)
     bne t5, s9, continua_valida_esquerda_baixo
     j valida_direita_baixo
@@ -526,7 +512,7 @@ salva_valida_esquerda_baixo:
     addi t5, t5, 1
     sw t5, (s1)
 valida_direita_baixo:
-    add s1, t6, s7 # j = aux + 1
+    add s1, t6, s7
     blt t1, s1, continua_for_calcula_bombas
     lw t5, (s1)
     bne t5, s9, continua_valida_direita_baixo
